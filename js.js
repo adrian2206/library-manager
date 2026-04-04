bc1884e5ee66cfd589520481f0ecac619fbcf9dc
adrian2206 <29433074+adrian2206@://github.com>
2026-04-04 15:50:25 +0300

    Add button in html. Add light style to button. Create function for adding new books in html table.

--- js.js
+++ js.js
@@ -1,9 +1,6 @@
 const myLibrary = [
-    { id: "a3f9b2c1-1234-4d56-8901-abcdef123456", title: "The Hobbit", author: "J.R.R. Tolkien", genre: "Fantasy", pages: 310, read: true },
-    { id: "b7c2d4e5-5678-4f90-1234-fedcba654321", title: "1984", author: "George Orwell", genre: "Dystopian", pages: 328, read: true },
-    { id: "c1e3f5a7-9012-4b34-5678-123456abcdef", title: "Dune", author: "Frank Herbert", genre: "Sci-Fi", pages: 412, read: false },
-    { id: "d4a6b8c0-3456-4c78-9012-abcdef789012", title: "The title of the Wind", author: "Patrick Rothfuss", genre: "Fantasy", pages: 662, read: true },
-    { id: "e2f8d1b3-7890-4e12-3456-654321fedcba", title: "Brave New World", author: "Aldous Huxley", genre: "Dystopian", pages: 311, read: false },
+    { id: "a3f9b2c1-1234-4d56-8901-abcdef123456", title: "The Hobbit", author: "J.R.R. Tolkien", genre: "Fantasy", pages: 310, read: 'Yes' },
+    { id: "c1e3f5a7-9012-4b34-5678-123456abcdef", title: "Dune", author: "Frank Herbert", genre: "Sci-Fi", pages: 412, read: 'No' },
 ];
 
 function Book(title, author, genre, pages,read) {
@@ -21,4 +18,31 @@ function Book(title, author, genre, pages,read) {
 
 function addBookToLibrary(title, author, genre, pages,read) {  
     myLibrary.push(new Book(title, author, genre, pages,read));
+}
+
+function displayLibrary() {
+    for(let i = 0; i < myLibrary.length; i++ ) {
+        const display = document.querySelector('#display');
+
+        const newSection = document.createElement('section');
+        newSection.classList.add('book');
+
+        const title = document.createElement('p');
+        title.textContent = `${myLibrary[i].title}`;
+
+        const author = document.createElement('p');
+        author.textContent = `${myLibrary[i].author}`;
+
+        const genre = document.createElement('p');
+        genre.textContent = `${myLibrary[i].genre}`;
+
+        const pages = document.createElement('p');
+        pages.textContent = `${myLibrary[i].pages}`;
+
+        const read = document.createElement('p');
+        read.textContent = `${myLibrary[i].read}`;
+
+        newSection.append(title, author, genre, pages, read);
+        display.append(newSection);
+    }
 }
